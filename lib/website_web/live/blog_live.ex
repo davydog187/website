1fecd0b53f1143e4c1533805db0025fa739b13b7
Marcelo Dominguez <marcelo@mimiquate.com>
2021-11-17 13:31:40 -0300

    Fix Typo

--- lib/website_web/live/blog_live.ex
+++ lib/website_web/live/blog_live.ex
@@ -17,7 +17,7 @@ defmodule WebsiteWeb.BlogLive do
       %{
         type: :talk,
         title: "Migrating from Kafka to RabbitMQ at Simplebet: Why and How",
-        descrioption: """
+        description: """
         At Simplebet, we are striving to make every moment of every sporting event a betting opportunity. In doing so, we initially chose Kafka to deliver market updates. The result was a setup which was difficult and expensive to maintain, and non-trivial for our customers to integrate with. After researching other delivery mechanisms, we migrated to RabbitMQ, as it provided us with a low-cost, low-latency alternative that satisfied our business needs. Best of all, it provided a familiar, standards-based means of integration for our customers with superior flexibility to what Kafka offers.
 
         In this talk, we will cover the technical and business reasons for why RabbitMQ has proven itself to be a great platform for building a B2B SaaS product, how it compares to other tools on the market, and where it excels in flexibility for our customers.

c69981f955e7d5d13cef516924219cceaa023b56
Dave Lucia <davelucianyc@gmail.com>
2021-11-26 14:24:18 -0500

    remove IO.inspects

--- lib/website/blog/markdown.ex
+++ lib/website/blog/markdown.ex
@@ -4,9 +4,6 @@ defmodule Website.Blog.Markdown do
   end
 
   def post_processor({"code", attrs, children, meta} = node) do
-    IO.inspect(attrs, label: "attr")
-    IO.inspect(children)
-    IO.inspect(meta, label: "meta")
     attrs = [{"class", "whatup"} | attrs]
     children = children |> to_string() |> Makeup.highlight()
 

cf2f96c1fcf8be64d3640fe01fc6b79a5cb676b3
Dave Lucia <davelucianyc@gmail.com>
2022-07-29 19:09:25 -0400

    Add filters on homepage

--- lib/website_web/live/blog_live/index.ex
+++ lib/website_web/live/blog_live/index.ex
@@ -7,6 +7,22 @@ defmodule WebsiteWeb.BlogLive.Index do
 
   alias Surface.Components.Link
 
+  def mount(_, _, socket) do
+    {:ok, assign(socket, filter: &Function.identity/1, filter_selected: :all)}
+  end
+
+  def handle_event("filter-content", %{"filter" => filter}, socket) do
+    filter_selected = String.to_existing_atom(filter)
+
+    filter =
+      case filter_selected do
+        :all -> &Function.identity/1
+        filter -> fn content -> content.type == filter_selected end
+      end
+
+    {:noreply, assign(socket, filter: filter, filter_selected: filter_selected)}
+  end
+
   def handle_params(params, _url, socket) do
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
   end

d10b2eb3f95933e023333b1151c7cac095471a68
Dave Lucia <davelucianyc@gmail.com>
2022-07-31 11:07:21 -0400

    Moar pineapple

--- lib/website_web/views/layout_view.ex
+++ lib/website_web/views/layout_view.ex
@@ -6,4 +6,15 @@ defmodule WebsiteWeb.LayoutView do
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
+
+  defp og_image(text) do
+    uri = URI.parse("https://og-image-bay-phi.vercel.app")
+
+    %URI{
+      uri
+      | path: "/" <> URI.encode(text || "Dave Lucia's Website"),
+        query: URI.encode_query(%{widths: "408", heights: "612"})
+    }
+    |> to_string
+  end
 end
